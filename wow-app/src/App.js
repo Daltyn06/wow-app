aee0b5465814462a8c72b03fb31e784605bf2f63
daltontyndall <dtyndall@symmetrimarketing.com>
2022-06-20 13:50:46 -0400

    Dynamic api call.

--- wow-app/src/App.js
+++ wow-app/src/App.js
@@ -4,31 +4,50 @@ import * as React from 'react'
 
 function App() {
   const [error, setError] = React.useState(null);
-  const [isLoaded, setIsLoaded] = React.useState(false);
+  const [data, setData] = React.useState(null);
+  const [loading, setLoading] = React.useState(true);
   const [items, setItems] = React.useState([]);
   const [gear, setGear] = React.useState([]);
   const [score, setScore] = React.useState([]);
   const [topDungeons, setTopDungeons] = React.useState([]);
+  const [name, setName] = React.useState([]);
+  const [server, setServer] = React.useState([]);
+  const url = `https://raider.io/api/v1/characters/profile?region=us&realm=${server}&name=${name}&fields=gear%2Cmythic_plus_scores_by_season%3Acurrent%2Cmythic_plus_best_runs%2C%20mythic_plus_weekly_highest_level_runs`;
+
 
   React.useEffect(() => {
-    fetch("https://raider.io/api/v1/characters/profile?region=us&realm=zuljin&name=orndain&fields=gear%2Cmythic_plus_scores_by_season%3Acurrent%2Cmythic_plus_best_runs%2C%20mythic_plus_weekly_highest_level_runs")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          setIsLoaded(true);
-          const data = JSON.parse(JSON.stringify(result));
-          setItems(data)
-          // console.log(result);
-          setGear(data.gear)
-          setScore(data.mythic_plus_scores_by_season)
-          setTopDungeons(data.mythic_plus_best_runs)
-        },
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
+    const getData = async () => {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(
+            `This is an HTTP error: The status is ${response.status}`
+          );
         }
-      )
-  }, [])
+        let actualData = await response.json();
+        setData(actualData);
+        setItems(actualData)
+        setGear(actualData.gear)
+        setScore(actualData.mythic_plus_scores_by_season)
+        setTopDungeons(actualData.mythic_plus_best_runs)
+        setError(null);
+      } catch(err) {
+        setError(err.message);
+        setData(null);
+      } finally {
+        setLoading(false);
+      }  
+    }
+    getData()
+  }, [url])
+
+  const slugify = str =>
+  str
+    .toLowerCase()
+    .trim()
+    .replace(/[^\w\s-]/g, '')
+    .replace(/[\s_-]+/g, '-')
+    .replace(/^-+|-+$/g, '');
 
   function padTo2Digits(num) {
     return num.toString().padStart(2, '0');
@@ -59,27 +78,56 @@ function App() {
     }
   }
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  } else if (!isLoaded) {
-    return <div>Loading...</div>;
-  } else {
+  function CharacterForm() {
+    function handleSubmit(event) {
+      event.preventDefault()
+      setName(event.currentTarget.elements.characterInput.value)
+      setServer(slugify(event.currentTarget.elements.characterServer.value))
+      console.log(slugify(event.currentTarget.elements.characterServer.value))
+    }
+    return (
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="characterServer">Realm</label>
+          <input id="characterServer" type="text" />
+          <label htmlFor="characterInput">Name</label>
+          <input id="characterInput" type="text" />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    )
+  }
+
+  function CharacterDetails() {
     return (
       <>
       <h1>{items.name} - {items.realm}</h1>
       <h3>{gear.item_level_equipped} - {items.active_spec_name} {items.class}</h3>
       <div style={{color: score[0].segments.all.color}}>{score[0].segments.all.score}</div>
-      
+
       {topDungeons.map(item => (
           <div key={item.index}>
             {item.dungeon} {item.mythic_level} <strong>{item.score}</strong> - {timerCalc(item.clear_time_ms, item.par_time_ms)}
           </div>
         ))}
-  
-        {/* <pre>{JSON.stringify(topDungeons, null, 2)}</pre> */}
+
+        {/* <pre>{JSON.stringify(items, null, 2)}</pre> */}
+        </>
+      );
+    }
+
+    return (
+      <>
+      {loading && <div>A moment please...</div>}
+      {error && (
+        <div>{`There is a problem fetching the post data - ${error}`}</div>
+      )}
+      <CharacterForm></CharacterForm>
+      {data && (
+        <CharacterDetails></CharacterDetails>
+      )}
       </>
     );
-  }
 }
 
 export default App;
